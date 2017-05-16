81b4645f383da875dfcc57f288688c7b6cea67a0
Kyle Steinle <kyledavid022@gmail.com>
2017-05-16 13:09:41 -0700

    updated responsive styles for team page

--- page-team.php
+++ page-team.php
@@ -7,10 +7,10 @@ get_header(); ?>
 			<div class="container team-container">
 				<h1 id="meet">Meet '<span class="initials">k</span>' and '<span class="initials">n</span>'</h1>
 
-				<div class="row">
+				<div class="row kyle-row">
 					<div class="background-shape triangle">
 					</div>
-					<div class="col col-sm-offset-1 col-sm-4">
+					<div class="col col-md-offset-1 col-md-4 col-sm-5">
 						<div class="figure bio-figure">
 							<img src="http://www.knwebwork.com/wp-content/uploads/2017/05/kyle-steinle-2-e1494448447940.jpg" alt="Las Vegas web developer Kyle Steinle" title="Las Vegas web developer Kyle Steinle"/>
 							<div class="fig-caption bio-caption">
@@ -43,16 +43,7 @@ get_header(); ?>
 				<div class="row second-team-row">
 					<div class="background-shape rectangle">
 					</div>
-					<div class="col col-sm-6">
-						<p class="biography">
-							<p><span class="leading-word">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Nam a tincidunt erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eget dictum lectus. Sed dapibus neque pulvinar leo dapibus suscipit. Nulla sit amet malesuada diam. Nulla non purus purus. Donec rutrum, risus eu tristique elementum, nisl lacus luctus elit, id condimentum nisi nunc sit amet est. Fusce blandit, metus a volutpat facilisis, velit urna aliquet tortor, ut molestie urna ante at enim. Praesent tempus metus eros, vel aliquam massa convallis id. Sed pulvinar et nisl at consectetur. Donec vitae nisi aliquet, pellentesque dui vitae, euismod orci.
-							</p>
-
-							<p>Ut eu hendrerit neque, sed mollis arcu. Donec at magna vel quam finibus congue elementum vel velit. Suspendisse auctor tempus risus non tristique. Cras lectus tellus, auctor non mi nec, vestibulum molestie erat. Suspendisse venenatis nisi vulputate, finibus sem eu, pellentesque dolor. Aenean elit nunc, facilisis ut lorem vel, euismod convallis tortor. Morbi felis enim, feugiat eu nibh vel, blandit euismod nisi. Proin viverra lorem vitae facilisis aliquet. Morbi vitae diam lectus. Nulla non arcu non tellus ullamcorper tristique ut quis urna.
-							</p>
-						</p>
-					</div>
-					<div class="col col-sm-offset-1 col-sm-4">
+					<div class="col col-md-offset-1 col-md-4 col-sm-5 col-sm-push-6">
 						<div class="figure bio-figure">
 							<img src="http://www.knwebwork.com/wp-content/uploads/2017/05/neal-hill-e1494448950873.jpg" alt="Las Vegas web developer Neal Hill" title="Las Vegas web developer Neal Hill"/>
 
@@ -71,6 +62,17 @@ get_header(); ?>
 							<a href="https://twitter.com/knwebwork" target="_blank">Twitter</a>
 						</div>
 					</div>
+
+					<div class="col col-sm-6 col-sm-pull-5">
+						<p class="biography">
+							<p><span class="leading-word">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Nam a tincidunt erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eget dictum lectus. Sed dapibus neque pulvinar leo dapibus suscipit. Nulla sit amet malesuada diam. Nulla non purus purus. Donec rutrum, risus eu tristique elementum, nisl lacus luctus elit, id condimentum nisi nunc sit amet est. Fusce blandit, metus a volutpat facilisis, velit urna aliquet tortor, ut molestie urna ante at enim. Praesent tempus metus eros, vel aliquam massa convallis id. Sed pulvinar et nisl at consectetur. Donec vitae nisi aliquet, pellentesque dui vitae, euismod orci.
+							</p>
+
+							<p>Ut eu hendrerit neque, sed mollis arcu. Donec at magna vel quam finibus congue elementum vel velit. Suspendisse auctor tempus risus non tristique. Cras lectus tellus, auctor non mi nec, vestibulum molestie erat. Suspendisse venenatis nisi vulputate, finibus sem eu, pellentesque dolor. Aenean elit nunc, facilisis ut lorem vel, euismod convallis tortor. Morbi felis enim, feugiat eu nibh vel, blandit euismod nisi. Proin viverra lorem vitae facilisis aliquet. Morbi vitae diam lectus. Nulla non arcu non tellus ullamcorper tristique ut quis urna.
+							</p>
+						</p>
+					</div>
+					
 				</div><!-- row -->
 
 			</div><!-- container -->
