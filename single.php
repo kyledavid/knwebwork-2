7ee6eacab4fae82f4d0b3c2906e245cfd64bc20d
Kyle Steinle <kyledavid022@gmail.com>
2017-06-02 22:54:45 -0700

    formatted basic blog index and single post pages

--- single.php
+++ single.php
@@ -10,26 +10,31 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
+			<div class="row">
+				<div class="col col-md-8">
 
-		<?php
-		while ( have_posts() ) : the_post();
+					<?php
+					while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+						get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+						the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-		endwhile; // End of the loop.
-		?>
-
+					endwhile; // End of the loop.
+					?>
+				</div>
+				<div class="col col-md-offset-1 col-md-3">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
