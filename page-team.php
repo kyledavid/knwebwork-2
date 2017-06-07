59c06c05cb57c2666e757e4ad7292f7f2777f185
Kyle Steinle <kyledavid022@gmail.com>
2017-06-07 14:22:45 -0700

    added Kyle's bio

--- page-team.php
+++ page-team.php
@@ -31,10 +31,18 @@ get_header(); ?>
 					
 					<div class="col-sm-offset-1 col col-sm-6">
 						<div class="biography">
-							<p><span class="leading-word">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Nam a tincidunt erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eget dictum lectus. Sed dapibus neque pulvinar leo dapibus suscipit. Nulla sit amet malesuada diam. Nulla non purus purus. Donec rutrum, risus eu tristique elementum, nisl lacus luctus elit, id condimentum nisi nunc sit amet est. Fusce blandit, metus a volutpat facilisis, velit urna aliquet tortor, ut molestie urna ante at enim. Praesent tempus metus eros, vel aliquam massa convallis id. Sed pulvinar et nisl at consectetur. Donec vitae nisi aliquet, pellentesque dui vitae, euismod orci.
+							<p><span class="leading-word">Kyle Steinle</span> has been in the web dev game from an early age, having built his first site back in the days of Geocities and free .tk domains. Those were dark times, but he carried on with only a mininum of mental scarring (though he is haunted by a recurring dream of blinking marquees). Preferring the front-end side of things, Kyle has a high level of skill when it comes to building modern, responsive websites, especially on the Wordpress platform. He is also an avid student of Javascript and has especially enjoyed experiments within the React framework.
+							</p>
+							<p>
+							Possessing a secondary interest in design (he designed and coded this entire site after all) Kyle has taken this perspective into his programming work. Its not enough for him to make code that simply works, instead, he strives for the ideal that someone that could hardly code at all would be able to look at his work and understand what's going on. His code tells a story, through variables and function names as well semantic use of HTML elements and CSS classes, that illuminates its intent and makes it supremely easy to understand and maintain.
 							</p>
 
-							<p>Ut eu hendrerit neque, sed mollis arcu. Donec at magna vel quam finibus congue elementum vel velit. Suspendisse auctor tempus risus non tristique. Cras lectus tellus, auctor non mi nec, vestibulum molestie erat. Suspendisse venenatis nisi vulputate, finibus sem eu, pellentesque dolor. Aenean elit nunc, facilisis ut lorem vel, euismod convallis tortor. Morbi felis enim, feugiat eu nibh vel, blandit euismod nisi. Proin viverra lorem vitae facilisis aliquet. Morbi vitae diam lectus. Nulla non arcu non tellus ullamcorper tristique ut quis urna.
+							<p>
+							When he's not coding, Kyle enjoys cooking but mostly eating, difficult video games (think Dark Souls), and rock climbing. You'll most often find him ambling through Kraft Mountain, crashpad strapped to his back, searching for his next big *send.
+							</p>
+
+							<p>
+							*That means climb for any of you non-climbing folks
 							</p>
 						</div>
 					</div>
@@ -64,13 +72,13 @@ get_header(); ?>
 					</div>
 
 					<div class="col col-sm-6 col-sm-pull-5">
-						<p class="biography">
+						<div class="biography">
 							<p><span class="leading-word">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Nam a tincidunt erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam eget dictum lectus. Sed dapibus neque pulvinar leo dapibus suscipit. Nulla sit amet malesuada diam. Nulla non purus purus. Donec rutrum, risus eu tristique elementum, nisl lacus luctus elit, id condimentum nisi nunc sit amet est. Fusce blandit, metus a volutpat facilisis, velit urna aliquet tortor, ut molestie urna ante at enim. Praesent tempus metus eros, vel aliquam massa convallis id. Sed pulvinar et nisl at consectetur. Donec vitae nisi aliquet, pellentesque dui vitae, euismod orci.
 							</p>
 
 							<p>Ut eu hendrerit neque, sed mollis arcu. Donec at magna vel quam finibus congue elementum vel velit. Suspendisse auctor tempus risus non tristique. Cras lectus tellus, auctor non mi nec, vestibulum molestie erat. Suspendisse venenatis nisi vulputate, finibus sem eu, pellentesque dolor. Aenean elit nunc, facilisis ut lorem vel, euismod convallis tortor. Morbi felis enim, feugiat eu nibh vel, blandit euismod nisi. Proin viverra lorem vitae facilisis aliquet. Morbi vitae diam lectus. Nulla non arcu non tellus ullamcorper tristique ut quis urna.
 							</p>
-						</p>
+						</div>
 					</div>
 					
 				</div><!-- row -->
